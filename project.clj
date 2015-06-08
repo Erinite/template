cec1d8a3792b5ebf105eeb3e8fadb7ff2317ee8a
danielytics <dkersten@gmail.com>
2015-06-08 18:38:56 +0100

    test/ only in source path in dev profile

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3308" :exclusions [org.clojure/clojure]]
                  [org.clojure/core.match "0.2.2"]]
-  :source-paths ["src" "test"]
   :profiles {
     :dev {
+      :source-paths ["src" "test"]
       :dependencies [[criterium "0.4.3"]]}})
